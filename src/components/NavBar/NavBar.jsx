f022785ffbde9d2338beea5405c980efeacec946
NgoMinhTam <ngominhtam26112005@gmail.com>
2025-02-20 13:34:05 +0700

    update navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,10 +2,18 @@ import React from 'react'
 
 const NavBar = (props) => {
   return (
-    <div className="h-20 flex justify-between bg-green-700 rounded-md">
+    <nav className="h-20 flex justify-between bg-green-700 rounded-md">
       <img src="public/Logo.png" alt="" className='p-5 border rounded-[40px]'/>
-      <div className="p-5">{props.children}</div>
-    </div>
+      <div className="p-5">
+      {React.Children.map(props.children, (child) =>
+          React.isValidElement(child) 
+            ? React.cloneElement(child, {
+                className: `${child.props.className || ""} no-underline p-4 text-yellow-100`,
+              })
+            : child
+        )}
+      </div>
+    </nav>
   )
 }
 
